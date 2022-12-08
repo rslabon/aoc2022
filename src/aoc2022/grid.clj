e3981ba20b8811de6809c8500ea2b7f993c8f6dd
r <r@r>
2022-12-08 22:54:39 +0100

    grid test

--- src/aoc2022/grid.clj
+++ src/aoc2022/grid.clj
@@ -1,6 +1,6 @@
 (ns aoc2022.grid)
 
-(defn rotate-right-90
+(defn grid-rotate-right-90
   [grid]
   (loop [column 0
          rotated-grid []]
@@ -11,7 +11,7 @@
     )
   )
 
-(defn rotate-left-90
+(defn grid-rotate-left-90
   [grid]
   (loop [column (dec (count (first grid)))
          rotated-grid []]
@@ -21,7 +21,7 @@
       )
     ))
 
-(defn merge-grid
+(defn grid-merge
   [grid1 grid2]
   (loop [rows1 grid1
          rows2 grid2
@@ -33,9 +33,61 @@
     )
   )
 
-(defn grid-2x
-  [grid]
-  (let [grid (merge-grid grid grid)
-        [top bottom] (repeat 2 grid)]
-    (into top bottom)
+(defn grid-increase
+  [times grid]
+  (let [grid (map #(reduce into [] (repeat times %)) grid)]
+    (reduce into [] (repeat times grid))
     ))
+
+(defn grid-get-item
+  [grid row column]
+  (if (or (> 0 row) (> 0 column) (>= row (count grid)) (>= column (count (first grid))))
+    nil
+    (nth (nth grid row) column)
+    ))
+
+(defn grid-get-items-up
+  ([grid row column]
+   (let [ids (for [i (range (dec row) -1 -1)] i)]
+     (mapv #(grid-get-item grid % column) ids)
+     ))
+  ([grid row column edge] (conj (grid-get-items-up grid row column) edge))
+  )
+
+(defn grid-get-items-down
+  ([grid row column]
+   (let [ids (for [i (range (inc row) (count grid))] i)]
+     (mapv #(grid-get-item grid % column) ids)
+     ))
+  ([grid row column edge] (conj (grid-get-items-down grid row column) edge))
+  )
+
+(defn grid-get-items-left
+  ([grid row column]
+   (let [ids (for [i (range (dec column) -1 -1)] i)]
+     (mapv #(grid-get-item grid row %) ids)
+     ))
+  ([grid row column edge] (conj (grid-get-items-left grid row column) edge))
+  )
+
+(defn grid-get-items-right
+  ([grid row column]
+   (let [ids (for [i (range (inc column) (count (first grid)))] i)]
+     (mapv #(grid-get-item grid row %) ids)
+     ))
+  ([grid row column edge] (conj (grid-get-items-right grid row column) edge))
+  )
+
+(defn grid-get-items-around
+  ([grid row column] (filterv (fn [[item]] (some? item)) (grid-get-items-around grid row column nil)))
+  ([grid row column edge]
+   (let [ids [[-1 -1] [-1 0] [-1 1] [0 -1] [0 1] [1 -1] [1 0] [1 1]]]
+     (mapv (fn [[item position]] (if (nil? item) [edge position] [item position]))
+           (mapv (fn [[i j]]
+                   (let [item-row (+ row i)
+                         item-column (+ column j)
+                         item (grid-get-item grid item-row item-column)]
+                     [item [item-row item-column]]))
+                 ids))
+     )
+   ))
--- test/aoc2022/grid_test.clj
+++ test/aoc2022/grid_test.clj
@@ -8,45 +8,111 @@
            [10 11 12 13 14]
            [15 16 17 18 19]])
 
-(deftest rotate-right-90-test
-  (testing "rotate-right-90"
-    (is (= (rotate-right-90 grid) [[15 10 5 0]
-                                   [16 11 6 1]
-                                   [17 12 7 2]
-                                   [18 13 8 3]
-                                   [19 14 9 4]]))
+(deftest grid-rotate-right-90-test
+  (testing "grid-rotate-right-90"
+    (is (= (grid-rotate-right-90 grid) [[15 10 5 0]
+                                        [16 11 6 1]
+                                        [17 12 7 2]
+                                        [18 13 8 3]
+                                        [19 14 9 4]]))
 
-    (is (= (rotate-right-90 (rotate-right-90 (rotate-right-90 (rotate-right-90 grid)))) grid))
+    (is (= (grid-rotate-right-90 (grid-rotate-right-90 (grid-rotate-right-90 (grid-rotate-right-90 grid)))) grid))
     )
   )
 
-(deftest rotate-left-90-test
-  (testing "rotate-left-90"
-    (is (= (rotate-left-90 grid) [[4 9 14 19]
-                                  [3 8 13 18]
-                                  [2 7 12 17]
-                                  [1 6 11 16]
-                                  [0 5 10 15]]))
+(deftest grid-rotate-left-90-test
+  (testing "grid-rotate-left-90"
+    (is (= (grid-rotate-left-90 grid) [[4 9 14 19]
+                                       [3 8 13 18]
+                                       [2 7 12 17]
+                                       [1 6 11 16]
+                                       [0 5 10 15]]))
 
-    (is (= (rotate-left-90 (rotate-left-90 (rotate-left-90 (rotate-left-90 grid)))) grid))
+    (is (= (grid-rotate-left-90 (grid-rotate-left-90 (grid-rotate-left-90 (grid-rotate-left-90 grid)))) grid))
     )
   )
 
-(deftest merge-grid-test
-  (testing "merge-grid"
-    (is (= (merge-grid grid grid) [[0 1 2 3 4 0 1 2 3 4]
+(deftest grid-merge-test
+  (testing "grid-merge"
+    (is (= (grid-merge grid grid) [[0 1 2 3 4 0 1 2 3 4]
                                    [5 6 7 8 9 5 6 7 8 9]
                                    [10 11 12 13 14 10 11 12 13 14]
                                    [15 16 17 18 19 15 16 17 18 19]]))
     )
   )
 
-(deftest grid-2x-test
-  (testing "grid-2x-grid"
-    (is (= (grid-2x [[1 2]
-                     [3 4]]) [[1 2 1 2]
-                              [3 4 3 4]
-                              [1 2 1 2]
-                              [3 4 3 4]]))
+(deftest grid-increase-test
+  (testing "grid-increase-grid"
+    (is (= (grid-increase 2 [[1 2]
+                             [3 4]]) [[1 2 1 2]
+                                      [3 4 3 4]
+                                      [1 2 1 2]
+                                      [3 4 3 4]]))
+
+    (is (= (grid-increase 3 [[1 2]
+                             [3 4]]) [[1 2 1 2 1 2]
+                                      [3 4 3 4 3 4]
+                                      [1 2 1 2 1 2]
+                                      [3 4 3 4 3 4]
+                                      [1 2 1 2 1 2]
+                                      [3 4 3 4 3 4]]))
+    )
+  )
+
+(deftest grid-get-items-up-test
+  (testing "grid-get-items-up"
+    (is (= (grid-get-items-up grid 0 0) []))
+    (is (= (grid-get-items-up grid 2 2) [7 2]))
+    (is (= (grid-get-items-up grid 2 2 -1) [7 2 -1]))
+    )
+  )
+
+(deftest grid-get-items-down-test
+  (testing "grid-get-items-down"
+    (is (= (grid-get-items-down grid 3 3) []))
+    (is (= (grid-get-items-down grid 1 2) [12 17]))
+    (is (= (grid-get-items-down grid 1 2 -1) [12 17 -1]))
+    )
+  )
+
+(deftest grid-get-items-left-test
+  (testing "grid-get-items-left"
+    (is (= (grid-get-items-left grid 0 0) []))
+    (is (= (grid-get-items-left grid 2 3) [12 11 10]))
+    (is (= (grid-get-items-left grid 2 3 -1) [12 11 10 -1]))
+    )
+  )
+
+(deftest grid-get-items-right-test
+  (testing "grid-get-items-right"
+    (is (= (grid-get-items-right grid 3 4) []))
+    (is (= (grid-get-items-right grid 2 1) [12 13 14]))
+    (is (= (grid-get-items-right grid 2 1 -1) [12 13 14 -1]))
+    )
+  )
+
+(deftest grid-get-items-around-test
+  (testing "grid-get-items-around"
+    (is (= (grid-get-items-around grid 1 1) [[0 [0 0]]
+                                             [1 [0 1]]
+                                             [2 [0 2]]
+                                             [5 [1 0]]
+                                             [7 [1 2]]
+                                             [10 [2 0]]
+                                             [11 [2 1]]
+                                             [12 [2 2]]]))
+
+    (is (= (grid-get-items-around grid 0 0) [[1 [0 1]]
+                                             [5 [1 0]]
+                                             [6 [1 1]]]))
+
+    (is (= (grid-get-items-around grid 0 0 -1) [[-1 [-1 -1]]
+                                                [-1 [-1 0]]
+                                                [-1 [-1 1]]
+                                                [-1 [0 -1]]
+                                                [1 [0 1]]
+                                                [-1 [1 -1]]
+                                                [5 [1 0]]
+                                                [6 [1 1]]]))
     )
   )
