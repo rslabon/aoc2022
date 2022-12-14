366fc73cbc3df137060912050cc98947789ac460
Rafal Slabon <rafal.slabon@ocado.com>
2022-12-14 15:30:22 +0100

    day14

---
+++ src/aoc2022/day14.clj
@@ -0,0 +1,130 @@
+(ns aoc2022.day14
+  (:require [clojure.string :as str]))
+
+(defn diff
+  [a b] (if (< a b) (range a (inc b)) (range a (dec b) -1)))
+
+(defn parse-line
+  [line]
+  (loop [line-coords (mapv #(mapv read-string (str/split % #",")) (str/split line #" -> "))
+         point-coords []]
+    (if (< (count line-coords) 2)
+      point-coords
+      (let [[start-x start-y] (first line-coords)
+            [end-x end-y] (second line-coords)
+            diff-x (diff start-x end-x)
+            diff-y (diff start-y end-y)
+            new-points (if (= (count diff-x) (count diff-y))
+                         (mapv (fn [i j] [i j]) diff-x diff-y)
+                         (for [x diff-x y diff-y] [x y])
+                         )
+            new-points (if (empty? point-coords)
+                         new-points
+                         (rest new-points)
+                         )]
+        (recur (rest line-coords) (into point-coords new-points))
+        )
+      )
+    ))
+
+(defn parse-grid
+  [sand-source input]
+  (let [rock-lines (mapv parse-line (filterv not-empty (str/split input #"\n")))
+        rock-points (reduce (fn [rock-points rock-line] (reduce (fn [m point] (conj m point)) rock-points rock-line)) #{} rock-lines)
+        sand-points #{}]
+    {:rock-points rock-points, :sand-points sand-points :sand-source sand-source}
+    )
+  )
+
+(defn grid-to-string
+  [grid]
+  (let [rock-points (mapv (fn [p] [p "#"]) (:rock-points grid))
+        sand-points (mapv (fn [p] [p "o"]) (:sand-points grid))
+        points (vec (conj (concat rock-points sand-points) [(:sand-source grid) "+"]))
+        min-x (- (apply min (mapv first (mapv first points))) 5)
+        max-x (apply max (mapv first (mapv first points)))
+        x-size (+ 10 (- max-x min-x))
+        y-size (+ 2 (apply max (mapv second (mapv first points))))]
+    (loop [points points
+           result (vec (repeat y-size (vec (repeat x-size "."))))]
+      (if (empty? points)
+        (str/join "\n" (mapv str/join result))
+        (recur
+          (rest points)
+          (let [[p point-char] (first points)]
+            (map-indexed (fn [row-idx row] (map-indexed (fn [col-idx cell]
+                                                          (if (= [(+ col-idx min-x) row-idx] p)
+                                                            point-char
+                                                            cell
+                                                            )) row)) result)
+            )
+          )
+        ))))
+
+(defn into-abyss?
+  [grid [_ y]]
+  (let [max-y (apply max (mapv second (:rock-points grid)))]
+    (> y max-y)
+    )
+  )
+
+(defn move-point
+  [[x y] grid]
+  (let [can-move? (fn [p] (not (or (contains? (:rock-points grid) p) (contains? (:sand-points grid) p))))
+        down [x (inc y)]
+        left-down [(dec x) (inc y)]
+        right-down [(inc x) (inc y)]]
+    (cond
+      (into-abyss? grid [x y]) nil
+      (can-move? down) (recur down grid)
+      (can-move? left-down) (recur left-down grid)
+      (can-move? right-down) (recur right-down grid)
+      :else [x y]
+      )
+    ))
+
+(defn drop-sand
+  ([grid]
+   (let [[sx sy] (:sand-source grid)
+         sand-point (move-point [sx sy] grid)]
+     (if (nil? sand-point)
+       grid
+       (update grid :sand-points conj sand-point)
+       )))
+  ([grid n] ((apply comp (repeat n drop-sand)) grid))
+  )
+
+(defn part1
+  [input]
+  (let [grid (parse-grid [500, 0] input)]
+    (loop [grid grid
+           n 0]
+      (let [next-grid (drop-sand grid)]
+        (if (= grid next-grid)
+          n
+          (recur next-grid (inc n))
+          )
+        )
+      )
+    ))
+
+(defn part2
+  [input]
+  (let [grid (parse-grid [500, 0] input)
+        y (+ 2 (apply max (mapv second (:rock-points grid))))
+        x-min (apply min (mapv first (:rock-points grid)))
+        x-max (apply max (mapv first (:rock-points grid)))
+        floor-points (mapv (fn [i] [i y]) (range (- x-min y) (+ x-max y)))
+        grid (update grid :rock-points into floor-points)]
+    (loop [grid grid
+           n 0]
+      (let [next-grid (drop-sand grid)]
+        (do
+          (if (= 0 (mod n 300)) (println "turn=" n))
+          (if (= grid next-grid)
+            (do (println (grid-to-string grid)) n)
+            (recur next-grid (inc n))
+            )
+          ))
+      )
+    ))
---
+++ test/aoc2022/day14_test.clj
@@ -0,0 +1,38 @@
+(ns aoc2022.day14-test
+  (:require [aoc2022.day14 :refer :all]
+            [clojure.test :refer :all]))
+
+(def puzzle-input (slurp "resources/day14.txt"))
+(def example "498,4 -> 498,6 -> 496,6\n503,4 -> 502,4 -> 502,9 -> 494,9")
+
+;(def grid (parse-grid [500, 0] puzzle-input))
+;(def grid (parse-grid [500, 0] example))
+;(println (grid-to-string grid))
+;(println (grid-to-string (drop-sand grid 27)))
+
+(deftest parse-line-test
+  (testing "parse-line"
+    (is (= (parse-line "0,0 -> 3,3") [[0 0] [1 1] [2 2] [3 3]]))
+    (is (= (parse-line "0,0 -> 0,3") [[0 0] [0 1] [0 2] [0 3]]))
+    (is (= (parse-line "10,5 -> 7,5") [[10 5] [9 5] [8 5] [7 5]]))
+    (is (= (parse-line "498,4 -> 498,6") [[498 4] [498 5] [498 6]]))
+    (is (= (parse-line "498,6 -> 496,6") [[498 6] [497 6] [496 6]]))
+    (is (= (parse-line "503,4 -> 502,4") [[503 4] [502 4]]))
+    (is (= (parse-line "502,9 -> 494,9") [[502 9] [501 9] [500 9] [499 9] [498 9] [497 9] [496 9] [495 9] [494 9]]))
+    (is (= (parse-line "498,4 -> 498,6 -> 496,6") [[498 4] [498 5] [498 6] [497 6] [496 6]]))
+    )
+  )
+
+(deftest part1-test
+  (testing "part1"
+    (is (= (part1 example) 24))
+    (is (= (part1 puzzle-input) 779))
+    )
+  )
+
+(deftest part2-test
+  (testing "part2"
+    ;(is (= (part2 example) 93))
+    (is (= (part2 puzzle-input) 27426))
+    )
+  )
