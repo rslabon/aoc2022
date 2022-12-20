5f2903a660523be3f82e5993f702c60d14c054c6
r <r@r>
2022-12-20 01:53:29 +0100

    day 19 not yet

--- src/aoc2022/day19.clj
+++ src/aoc2022/day19.clj
@@ -2,38 +2,107 @@
 
 (defn collected-inc
   [collected robots]
-  (map + collected robots)
+  (reduce (fn [m type] (update m type + (get robots type))) collected (keys collected))
+  )
+
+(defn consume-collected-for-build
+  [collected robot-blueprint]
+  (apply hash-map (flatten (map (fn [[k v]] [k (- (get collected k) v)]) robot-blueprint)))
   )
 
 (defn can-build-robot?
-  [collected robot-blueprint]
-  (every? #(>= % 0) (map - collected robot-blueprint))
-  )
-
-(defn start-building
-  [collected robot-blueprint]
-  (map - collected robot-blueprint)
-  )
+  [collected blueprint type]
+  (let [robot-blueprint (get blueprint type)]
+    (every? #(>= % 0) (vals (consume-collected-for-build collected robot-blueprint)))
+    ))
 
 (defn build-robot
-  [blueprint collected robots robot-type]
-  (let [robot-blueprint (get blueprint robot-type)]
-    (if (can-build-robot? collected robot-blueprint)
-      [(start-building collected robot-blueprint)
-       (map + robots (condp = robot-type
-                       :ore [1 0 0 0]
-                       :cly [0 1 0 0]
-                       :obsidian [0 0 1 0]
-                       :geocode [0 0 0 1]))]
+  [blueprint collected robots type]
+  (let [robot-blueprint (get blueprint type)]
+    (if (can-build-robot? collected blueprint type)
+      [(consume-collected-for-build collected robot-blueprint) (update robots type + 1)]
       [collected robots]
       )
     )
   )
 
-(defn turn
-  [minute collected robots]
-  (if (= minute 24)
-    collected
-    0
+(def turn
+  (memoize
+    (fn
+      [minute collected robots blueprint]
+      (let [geode-blueprint (get blueprint :geode)
+            obsidian-blueprint (get blueprint :obsidian)
+            cly-blueprint (get blueprint :cly)]
+        (do
+          (println "minute=" minute " collected=" collected " robots=" robots)
+          (cond (= minute 24) (:geode collected)
+                (and (>= minute 21) (= (:geode robots) 0)) (:geode collected)
+                ;(and (>= minute 15) (= (:obsidian robots) 0)) (:geode collected)
+                ;(and (>= minute 18) (= (:obsidian robots) 1)) (:geode collected)
+                ;(and (>= minute 10) (<= (:cly robots) 1)) (:geode collected)
+                (>= (:ore robots) 8) (:geode collected)
+                (>= (:cly robots) 8) (:geode collected)
+                (>= (:obsidian robots) 8) (:geode collected)
+
+                (and (= (:geode robots) 0) (< 1 (- (get collected :ore) (get geode-blueprint :ore))) (< 1 (- (get collected :obsidian) (get geode-blueprint :obsidian))))
+                (:geode collected)
+
+                (and (= (:geode robots) 0) (= (:obsidian robots) 0) (< 1 (- (get collected :ore) (get obsidian-blueprint :ore))) (< 1 (- (get collected :cly) (get obsidian-blueprint :cly))))
+                (:geode collected)
+
+                (and (= (:obsidian robots) 0) (= (:cly robots) 0) (< 1 (- (get collected :ore) (get obsidian-blueprint :ore))))
+                (:geode collected)
+
+                :else
+                (let [chooses [(turn (+ minute 1) (collected-inc collected robots) robots blueprint)
+                               (let [[collected new-robots] (build-robot blueprint collected robots :geode)]
+                                 (if (= new-robots robots)
+                                   -1
+                                   (turn (+ minute 1) (collected-inc collected robots) new-robots blueprint)
+                                   ))
+                               (let [[collected new-robots] (build-robot blueprint collected robots :ore)]
+                                 (if (= new-robots robots)
+                                   -1
+                                   (turn (+ minute 1) (collected-inc collected robots) new-robots blueprint)))
+                               (let [[collected new-robots] (build-robot blueprint collected robots :cly)]
+                                 (if (= new-robots robots)
+                                   -1
+                                   (turn (+ minute 1) (collected-inc collected robots) new-robots blueprint)))
+                               (let [[collected new-robots] (build-robot blueprint collected robots :obsidian)]
+                                 (if (= new-robots robots)
+                                   -1
+                                   (turn (+ minute 1) (collected-inc collected robots) new-robots blueprint)))]
+
+                      ]
+                  (apply max (flatten chooses))
+                  )
+                )
+          )))))
+
+(defn parse-blueprint
+  [line]
+  (let [[_ id ore-robot cly-robot obsidian-robot-ore obsidian-robot-cly geode-robot-ore geode-robot-obsidian] (re-matches #"Blueprint (\d+): Each ore robot costs (\d+) ore\. Each clay robot costs (\d+) ore\. Each obsidian robot costs (\d+) ore and (\d+) clay\. Each geode robot costs (\d+) ore and (\d+) obsidian\." line)]
+    {:id       (read-string id)
+     :ore      {:ore (read-string ore-robot) :cly 0 :obsidian 0 :geode 0}
+     :cly      {:ore (read-string cly-robot) :cly 0 :obsidian 0 :geode 0}
+     :obsidian {:ore (read-string obsidian-robot-ore) :cly (read-string obsidian-robot-cly)}
+     :geode    {:ore (read-string geode-robot-ore) :cly 0 :obsidian (read-string geode-robot-obsidian) :geode 0}
+     }
     )
   )
+
+(defn part1
+  [blueprints]
+  (let [result (map (fn [blueprint]
+                      [(:id blueprint)
+                       (turn
+                         0
+                         {:ore 0 :cly 0 :obsidian 0 :geode 0}
+                         {:ore 1 :cly 0 :obsidian 0 :geode 0}
+                         blueprint
+                         )]) blueprints)
+        ]
+    (reduce (fn [acc [id geode]] (+ acc (* id geode))) 0 result)
+    )
+  )
+
--- test/aoc2022/day19_test.clj
+++ test/aoc2022/day19_test.clj
@@ -1,25 +1,87 @@
 (ns aoc2022.day19-test
-  (:require [clojure.test :refer :all])
-  (:require [aoc2022.day19 :refer :all]))
+  (:require [aoc2022.day19 :refer :all]
+            [clojure.test :refer :all]))
 
-(def blueprint
+(def puzzle-input (slurp "resources/day19.txt"))
+
+(def blueprint-1
   {
-   :ore [4 0 0 0]
-   :cly [2 0 0 0]
-   :obsidian [3 14 0 0]
-   :geocode [2 0 7 0]
+   :id       1
+   :ore      {:ore 4 :cly 0 :obsidian 0 :geode 0}
+   :cly      {:ore 2 :cly 0 :obsidian 0 :geode 0}
+   :obsidian {:ore 3 :cly 14 :obsidian 0 :geode 0}
+   :geode  {:ore 2 :cly 0 :obsidian 7 :geode 0}
+   }
+  )
+
+(def blueprint-2
+  {
+   :id       2
+   :ore      {:ore 2 :cly 0 :obsidian 0 :geode 0}
+   :cly      {:ore 3 :cly 0 :obsidian 0 :geode 0}
+   :obsidian {:ore 3 :cly 8 :obsidian 0 :geode 0}
+   :geode  {:ore 3 :cly 0 :obsidian 12 :geode 0}
    }
   )
 
 (deftest can-build-robot?-test
   (testing "can-build-robot?"
-  (is (= (can-build-robot? [3 12 0 0] (:obsidian blueprint)) false))
-  (is (= (can-build-robot? [3 14 0 0] (:obsidian blueprint)) true))
-  (is (= (can-build-robot? [3 12 0 0] (:ore blueprint)) false))
-  ))
+    (is (= (can-build-robot? {:ore 3 :cly 12 :obsidian 0 :geode 0} blueprint-1 :obsidian) false))
+    (is (= (can-build-robot? {:ore 3 :cly 14 :obsidian 0 :geode 0} blueprint-1 :obsidian) true))
+    (is (= (can-build-robot? {:ore 3 :cly 12 :obsidian 0 :geode 0} blueprint-1 :ore) false))
+    ))
 
 (deftest build-robot-test
   (testing "build-robot"
-    (is (= (build-robot blueprint [5 1 0 0] [1 0 0 0] :ore) [[1 1 0 0] [2 0 0 0]]))
-    (is (= (build-robot blueprint [3 1 0 0] [1 0 0 0] :ore) [[1 0 0 0] [3 1 0 0]]))
+    (is (= (build-robot blueprint-1
+                        {:ore 5 :cly 1 :obsidian 0 :geode 0}
+                        {:ore 1 :cly 0 :obsidian 0 :geode 0} :ore)
+           [{:ore 1 :cly 1 :obsidian 0 :geode 0}
+            {:ore 2 :cly 0 :obsidian 0 :geode 0}]))
+    (is (= (build-robot blueprint-1
+                        {:ore 3 :cly 1 :obsidian 0 :geode 0}
+                        {:ore 1 :cly 0 :obsidian 0 :geode 0} :ore)
+           [{:ore 3 :cly 1 :obsidian 0 :geode 0}
+            {:ore 1 :cly 0 :obsidian 0 :geode 0}]))
+    ))
+
+(deftest collected-inc-test
+  (testing "collected-inc"
+    (is (= (collected-inc {:ore 0 :cly 0 :obsidian 0 :geode 0} {:ore 1 :cly 0 :obsidian 0 :geode 0}) {:ore 1 :cly 0 :obsidian 0 :geode 0}))
+    ))
+
+(deftest turn-test
+  (testing "turn"
+    (is (= (turn 0
+                 {:ore 0 :cly 0 :obsidian 0 :geode 0}
+                 {:ore 1 :cly 0 :obsidian 0 :geode 0}
+                 blueprint-1)
+           9))
+    ))
+
+(deftest parse-blueprint-test
+  (testing "parse-blueprint"
+    (is (= (parse-blueprint "Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 14 clay. Each geode robot costs 3 ore and 16 obsidian.")
+           {:cly      {:cly      0
+                       :geode  0
+                       :obsidian 0
+                       :ore      4}
+            :geode  {:cly      0
+                       :geode  0
+                       :obsidian 16
+                       :ore      3}
+            :id       1
+            :obsidian {:cly 14
+                       :ore 4}
+            :ore      {:cly      0
+                       :geode  0
+                       :obsidian 0
+                       :ore      4}}
+           ))
+    )
+  )
+
+(deftest part1-test
+  (testing "part1"
+    (is (= (part1 [blueprint-2]) 33))
     ))
